59ea80824b3105f149b04fe8ef257240b94f8be9
pa <prateek.ashtikar512@yahoo.in>
2016-08-30 11:38:02 +0530

    Spring Boot - CHAP-02

---
+++ springboot-basic-chap-02/src/main/java/com/sivalabs/demo/SpringbootBasicApplication.java
@@ -0,0 +1,11 @@
+package com.sivalabs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasicApplication {
+	public static void main(String[] args){
+		SpringApplication.run(SpringbootBasicApplication.class, args);
+	}
+}
